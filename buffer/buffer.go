a0538208c609ad9bef6eb88d0a6e089584642ec8
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-11-17 15:51:05 -0500

    text inserts for single bytes and strings

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -1,14 +1,23 @@
 package buffer
 
 type GapBuffer struct {
-	buff []byte
+	buff      []byte
+	gapSize   int
+	buffStart int
+	buffEnd   int
 }
 
 // Method to create a new buffer and initialize in to a valid state
 // Takes a string as a parameter and returns a pointer to the created buffer
 func New(source []byte) *GapBuffer {
+	size := 20
+	contents := append(source, make([]byte, size)...)
+
 	buffer := &GapBuffer{
-		buff: source,
+		buff:      contents,
+		gapSize:   size,
+		buffStart: len(source),
+		buffEnd:   len(contents),
 	}
 	return buffer
 }
@@ -16,5 +25,41 @@ func New(source []byte) *GapBuffer {
 // Method to return the buffers contents
 // Takes no parameters and returns a string
 func (b *GapBuffer) ToString() string {
-	return string(b.buff)
+	left := string(b.buff[:b.buffStart])
+	right := string(b.buff[b.buffEnd:])
+	return left + right
+}
+
+// Method to insert characters into the buffer
+func (b *GapBuffer) InsertChar(char byte) {
+	if b.gapSize <= 1 {
+		b.grow()
+	}
+
+	b.buff[b.buffStart] = char
+	b.buffStart += 1
+	b.gapSize -= 1
+}
+
+func (b *GapBuffer) InsertString(text string) {
+	for idx := range text {
+		b.InsertChar(text[idx])
+	}
+}
+
+// Method to grow the gap size
+// Takes no arguments and return nothing
+// A bit janky but I think it does the job
+func (b *GapBuffer) grow() {
+	tmpLeft := b.buff[:b.buffStart]
+	tmpRight := b.buff[b.buffEnd:]
+	newGap := make([]byte, (b.size() * 2))
+	b.buff = append(b.buff, tmpRight...)
+	b.buff = append(b.buff, newGap...)
+	b.buff = append(b.buff, tmpLeft...)
+	b.gapSize = len(newGap)
+}
+
+func (b *GapBuffer) size() int {
+	return len(b.buff) - b.gapSize
 }
--- cmd/hydro/main.go
+++ cmd/hydro/main.go
@@ -8,12 +8,26 @@ import (
 )
 
 func main() {
-
 	if len(os.Args) < 2 {
 		fmt.Printf("Usage: %s [FILE PATH]\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	buffer := buffer.New(fileio.ReadFile(os.Args[1]))
-	fmt.Print(buffer.ToString())
+	fmt.Println(buffer.ToString())
+
+	buffer.InsertChar('a')
+	buffer.InsertChar('1')
+	buffer.InsertChar('1')
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+	buffer.InsertString(" hello world!")
+
+	fmt.Println(buffer.ToString())
 }
